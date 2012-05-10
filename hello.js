254af87fba439ebe1f56d72e9e1f40ace72d36c0
peter-mouland <peter.mouland@gmail.com>
2012-05-10 23:02:42 +0100

    init

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-/**
- * Created with IntelliJ IDEA.
- * User: uni_nake
- * Date: 10/05/2012
- * Time: 22:36
- * To change this template use File | Settings | File Templates.
- */
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Node.js\n');
+}).listen(8124, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8124/');
