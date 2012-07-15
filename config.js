777cf05f58c2083ed048b6d3ca137ea8bd1cf977
peter-mouland <peter.mouland@gmail.com>
2012-07-15 14:18:16 +0100

    with stylus + nib

--- config.js
+++ config.js
@@ -1,8 +1,18 @@
 var express = require('express'),
     app = express.createServer(),
-    jade = require('jade');
+    jade = require('jade'),
+    stylus = require('stylus'),
+    nib = require('nib'),
+    compile = function(str, path) {
+        return stylus(str)
+            .set('filename', path)
+            .use(nib())
+    };
 
 app.use(app.router);
+app.use(express.logger('dev'))
+app.use(stylus.middleware({ src: __dirname + '/public', compile: compile}));
+
 app.configure('development',function(){
     app.use(express.static(__dirname + '/public'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
