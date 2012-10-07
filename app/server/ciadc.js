ac44c2493f52fa2a91508bb4a7adb2a808f35c4b
peter-mouland <peter.mouland@gmail.com>
2012-10-07 18:04:52 +0100

    add data files back in

--- app/server/ciadc.js
+++ app/server/ciadc.js
@@ -1,8 +1,8 @@
 var fs = require('fs'),
     data = {
-        posts: {},//eval(fs.readFileSync('ciadc.posts.json')+'').sort(function(a,b){return (b.published && a.published != b.published);}),
-        index: {},//eval(fs.readFileSync('ciadc.index.json')+''),
-        admin: {}//eval(fs.readFileSync('ciadc.admin.json')+'')
+        posts: eval(fs.readFileSync('app/server/ciadc.posts.json')+'').sort(function(a,b){return (b.published && a.published != b.published);}),
+        index: eval(fs.readFileSync('app/server/ciadc.index.json')+''),
+        admin: eval(fs.readFileSync('app/server/ciadc.admin.json')+'')
     };
 
 module.exports = {
