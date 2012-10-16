ca67ad9473d08c6415a8d82e9fe7b249a12bfc6f
peter-mouland <peter.mouland@gmail.com>
2012-10-17 00:35:41 +0100

    use correct env settings for node. duh.

--- app.js
+++ app.js
@@ -17,11 +17,10 @@ var express = require('express'),
 
 app.root = __dirname;
 var port = process.env.PORT || 3000;
-var mongoUri = process.env.MONGOLAB_URI;
 
 require('./app/config')(app, express);
 require('./app/server/router')(app);
 
-app.listen(port || 3000, function(){
+app.listen(port, function(){
 //    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
